060539a6e7fbc02ae9f248a29a49f2c1db388128
xim2016 <xj_ma@yahoo.com>
2026-01-11 14:55:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -208,16 +208,6 @@ citation_page = st.Page(
 
 # -- NAVIGATION --
 
-# pg = st.navigation(
-#     {
-#         "": [home_page, datasets_page],
-#         "Analysis of Individual Samples": [metaprogram_page, metaprogram_feature_page, gene_page, s_tf_page, s_pathway_page , drug2cell_page],  # ligand_page,
-#         "Comparison Across Samples": [mp_specific_page,  heatmap_gene_correlation_page], 
-#         "Resources": [citation_page, contact_page]
-#     }
-# )
-
-# pg.run()
 
 pg = st.navigation(
     {
@@ -227,13 +217,6 @@ pg = st.navigation(
         "Resources": [citation_page, contact_page]
     }
 )
-
-# Safe default attribute check
-for page_group, pages in pg.pages.items():
-    for page in pages:
-        if getattr(page, "_default", False):
-            pg.current_page = page
-            break
 
 pg.run()
 
