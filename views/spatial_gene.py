ebc3a35015498c073d6bbe1c3212c4f60066b5b6
xim2016 <xj_ma@yahoo.com>
2026-05-03 23:22:23 -0400

    Fix typo in columns function call

--- views/spatial_gene.py
+++ views/spatial_gene.py
@@ -96,7 +96,7 @@ else:
     image_spatial = f"{IMG_REPO_ren}/{option2}/{option}.png"
   else:
     image_spatial = f"{IMG_REPO_son}/{option2}/{option}.png"
-  _,c,_ = st.colunmns([1,2,1])
+  _,c,_ = st.columns([1, 2, 1])
   if url_is_alive(image_spatial):
       c.image(image_spatial)
   else:
