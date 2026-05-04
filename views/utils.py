72346ae5213f2539139d1dc3a0aa377f264a8935
xim2016 <xj_ma@yahoo.com>
2026-05-03 23:25:37 -0400

    Implement URL reachability check function
    Added a function to check if a URL is reachable.

--- views/utils.py
+++ views/utils.py
@@ -14,3 +14,17 @@ def get_sample_metaprograms(filepath):
     with open(filepath, 'rb') as f:
         d = pickle.load(f)
     return(d)
+
+def url_is_alive(url):
+    """
+    Checks that a given URL is reachable.
+    :param url: A URL
+    :rtype: bool
+    """
+    request = urllib.request.Request(url)
+    request.get_method = lambda: 'HEAD'
+    try:
+        urllib.request.urlopen(request)
+        return True
+    except urllib.request.HTTPError:
+        return False
