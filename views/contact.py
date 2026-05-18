80e3379990e67310e7ac9cba8bcac4a0fe571d14
xim2016 <xj_ma@yahoo.com>
2026-05-18 12:55:20 -0400

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -5,8 +5,8 @@ st.write("")
 
 st.markdown("<h3 style='text-align: center; color: black; font-size:25px;'>The Department of Biomedical Informatics (DBMI) </h3>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: black; font-size:25px;'>University of Pittsburgh School of Medicine</h3>", unsafe_allow_html=True)
-
-st.markdown("""<div style="width: 100%"><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=5051%20Centre%20Avenue%20Pittsburgh,%20PA%2015213+(My%20Business%20Name)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe></div>""", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: black; font-size:25px;'>3422 Forbes Ave, Pittsburgh PA 15213</h3>", unsafe_allow_html=True)
+st.markdown("""<div style="width: 100%"><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=3422%20Forbes%20Avenue%20Pittsburgh,%20PA%2015213+(My%20Business%20Name)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe></div>""", unsafe_allow_html=True)
 st.write("")
 
 b, c = st.columns([ .7, .0001])
