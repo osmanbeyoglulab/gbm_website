2072826c05961117816f992bd0837e9e4459306e
xim2016 <xj_ma@yahoo.com>
2026-05-20 13:26:27 -0400

    Update spatial_pathway.py

--- views/spatial_pathway.py
+++ views/spatial_pathway.py
@@ -9,6 +9,7 @@ st.write("")
 st.info("View spatially inferred pathway activities across glioblastoma samples using the SPAN framework. Explore how major signaling and metabolic pathways—such as hypoxia, proliferation, and immune signaling—are distributed within the tumor microenvironment. Overlay pathway maps on histology images to uncover spatial heterogeneity and identify pathway activation linked to specific malignant or non-malignant programs. Comparative violin plots let you explore pathway variation across metaprograms. Use the search box and sample selector to navigate.")
 
 IMG_REPO = 'https://raw.githubusercontent.com/matthewlu2/gbm_small_data/main/spatial_pw_tab/'
+
 IMG_REPO_ravi = 'https://raw.githubusercontent.com/osmanbeyoglulab/gbm_data_v2/main/ravi_pathway'
 IMG_REPO_ravi_violin = 'https://raw.githubusercontent.com/osmanbeyoglulab/gbm_data_v2/main/ravi_pathway_violin'
 IMG_REPO_ren = 'https://raw.githubusercontent.com/osmanbeyoglulab/gbm_data_v2/main/ren_pathway'
@@ -63,7 +64,7 @@ if option not in samples_ren + samples_son:
   # b.image(f'{IMG_REPO}/violin_pw_activity/HALLMARK_{option2}/{option}.png')
   
   
-  IMG_REPO2 = 'https://raw.githubusercontent.com/osmanbeyoglulab/gbm_data/main'
+  IMG_REPO2 = 'https://raw.githubusercontent.com/osmanbeyoglulab/gbm_data_v2/main'
   st.markdown("<h3 style='text-align: center; color: black;'>Pathway Activity across Metaprograms</h1>", unsafe_allow_html=True)
   st.image(f'{IMG_REPO2}/across_metaprogram_top_pathways_per_sample/{option}.png')
 
